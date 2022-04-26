50c7bd588f990176d606b0a1a23e0e7583255f3f
Nlof72 <nlof142@gmail.com>
2022-04-26 20:08:08 +0700

    something

--- ITinder/Controllers/AuthController.swift
+++ ITinder/Controllers/AuthController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import Alamofire
+
+struct RegisterData: Decodable{
+    let accessToken: String
+    let accessTokenExpiredAt: String
+    let refreshToken: String
+    let refreshTokenExpiredAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken
+        case accessTokenExpiredAt
+        case refreshToken
+        case refreshTokenExpiredAt
+      }
+}
+
 
 class AuthController: UIViewController {
-
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet weak var ConfirmPasswordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func OnRegisterButtonClick(_ sender: UIButton) {
+        if let email = EmailField.text, let password = PasswordField.text{
+            registerUser(email: email, password: password)
+        }
     }
-    */
+    
+    @IBAction func OnLoginClick(_ sender: UIButton) {
+        
+    }
+    
+    func registerUser(email: String, password: String){
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password,
+        ]
+        
+        debugPrint(parameters)
 
+        AF.request("http://193.233.85.238/itindr/api/mobile/v1/auth/register", method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseDecodable(of: RegisterData.self){
+            response in
+            guard let data = response.value else { return }
+            debugPrint("Response: \(data.accessToken)")
+            print(data)
+        }
+    }
 }
