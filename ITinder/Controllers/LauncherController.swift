9ac2f3f1ddc42b1e0194165dbcfcf48a6ce9964f
Nlof72 <72521417+Nlof72@users.noreply.github.com>
2022-04-26 18:31:57 +0700

    Init

---
+++ ITinder/Controllers/LauncherController.swift
@@ -0,0 +1,18 @@
+//
+//  LauncherController.swift
+//  ITinder
+//
+//  Created by Nikita on 26.04.2022.
+//
+
+import UIKit
+
+class LauncherController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ ITinder/Helpers/Gradient.swift
@@ -0,0 +1,34 @@
+//
+//  Gradient.swift
+//  ITinder
+//
+//  Created by Nikita on 26.04.2022.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+ @IBInspectable var firstColor: UIColor = UIColor.clear {
+   didSet {
+       updateView()
+    }
+ }
+ @IBInspectable var secondColor: UIColor = UIColor.clear {
+    didSet {
+        updateView()
+    }
+ }
+    
+ override class var layerClass: AnyClass {
+    get {
+        return CAGradientLayer.self
+    }
+ }
+func updateView() {
+    let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map{$0.cgColor}
+    }
+}
+
